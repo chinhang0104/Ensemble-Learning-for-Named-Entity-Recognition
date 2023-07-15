20cbd3898abce031a215dcc5245a41f0333c2a47
chinhang0104 <57591312+chinhang0104@users.noreply.github.com>
2023-07-15 14:43:50 -0700

    Rename demo.py to class_demo.py

--- class_demo.py
+++ class_demo.py
@@ -6,8 +6,8 @@ import Stanza
 import Spacy
 import Nltk
 import Lac
-import Hanlp
-import Scoring
+#import Hanlp
+import VotingClassifier
 
 # Create intances of models
 stanza_instance = Stanza.Stanza()
@@ -35,19 +35,19 @@ for model in models:
     companies.append(model.get_company_name())
 
 # Finding the suitable entities by ensemble laerning
-scoring_instance = Scoring(models)
+voting_instance = VotingClassifier.VotingClassifier(models)
 # Human first
-scoring_instance.build_scoring_matrix(humans)
-# Different scoring methods
-human_found = scoring_instance.highest_score()
-#human_found = scoring_instance.percentage_within_highest_score()
-#human_found = scoring_instance.minimum_weighted_score()
+voting_instance.build_voting_matrix(humans)
+# Different voting methods
+human_found = voting_instance.highest_score()
+#human_found = voting_instance.percentage_within_highest_score()
+#human_found = voting_instance.minimum_weighted_score()
 # Company after
-scoring_instance.build_scoring_matrix(companies)
-# Different scoring methods
-company_found = scoring_instance.highest_score()
-#company_found = scoring_instance.percentage_within_highest_score()
-#company_found = scoring_instance.minimum_weighted_score()
+voting_instance.build_voting_matrix(companies)
+# Different voting methods
+company_found = voting_instance.highest_score()
+#company_found = voting_instance.percentage_within_highest_score()
+#company_found = voting_instance.minimum_weighted_score()
 
 # Write the result to a json file
 json_to_write = json.dumps({"humans": human_found, "companies": company_found})
