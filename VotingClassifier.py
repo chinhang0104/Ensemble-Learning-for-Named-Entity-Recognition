036f9ed6826fbf64384f94af7f57890fa2e107cc
chinhang0104 <57591312+chinhang0104@users.noreply.github.com>
2023-07-15 14:43:28 -0700

    Rename class Scoring to VotingClassifier.

--- VotingClassifier.py
+++ VotingClassifier.py
@@ -1,11 +1,12 @@
 from typing import Optional
-class Scoring:
-    # Scoring and weighting
+class VotingClassifier:
+    # Class to handle voting of ensemble learning.
     
+    # @param _models list of model instances
     def __init__(self, _models: Optional[list] = None):
         self.models = _models
         self.summed_weight = 0
-        self.scoring_matrix = None
+        self.Voting_matrix = None
     
     def __str__(self) -> str:
         return f"Loaded models: {self.models}"
@@ -29,50 +30,50 @@ class Scoring:
     # for example, return from Model.get_human_name()
     # @param _models list of model instances
     # @return The dictionary that has key-value pairs represcenting word and its total score.
-    def build_scoring_matrix(self, recognized_results: list, _models: Optional[list] = None) -> dict:
+    def build_voting_matrix(self, recognized_results: list, _models: Optional[list] = None) -> dict:
         if _models == None:
             _models = self.models        
         # Summing up score for each word
-        _scoring_matrix = {}
+        _Voting_matrix = {}
         for i in range(len(recognized_results)):
             for word in recognized_results[i]:
-                if word in _scoring_matrix:
-                    _scoring_matrix[word] += _models[i].weight
+                if word in _Voting_matrix:
+                    _Voting_matrix[word] += _models[i].weight
                 else:
-                    _scoring_matrix[word] = _models[i].weight
-        self.scoring_matrix = _scoring_matrix
-        return _scoring_matrix
+                    _Voting_matrix[word] = _models[i].weight
+        self.Voting_matrix = _Voting_matrix
+        return _Voting_matrix
     
     ## @brief
-    # Find the list of entities that has highest score.
-    # @param _scoring_matrix Dictionary of word, score pair. Default is self.scoring_matrix.
+    # Weighted Average Probabilities (Soft Voting as sklearn). Can used as Majority Voting (Hard Voting as sklearn) when are models' weight are set as equal.
+    # @param _Voting_matrix Dictionary of word, score pair. Default is self.Voting_matrix.
     # @return The list of entities.    
-    def highest_score(self, _scoring_matrix: Optional[dict] = None) -> list:
-        if _scoring_matrix == None:
-            _scoring_matrix = self.scoring_matrix
-        return [key for key, value in _scoring_matrix.items() if value == max(_scoring_matrix.values())]
+    def weighted_average_probabilities(self, _Voting_matrix: Optional[dict] = None) -> list:
+        if _Voting_matrix == None:
+            _Voting_matrix = self.Voting_matrix
+        return [key for key, value in _Voting_matrix.items() if value == max(_Voting_matrix.values())]
     
     ## @brief
     # Find the list of entities that has score within percentage of highest score.
     # @param percentage The percentage. For example, 0.8 means find all entities that has score at least 
     # 80 percent of highest score.
-    # @param _scoring_matrix Dictionary of word, score pair. Default is self.scoring_matrix.
+    # @param _Voting_matrix Dictionary of word, score pair. Default is self.Voting_matrix.
     # @return The list of entities.     
-    def percentage_within_highest_score(self, percentage = 0.8, _scoring_matrix: Optional[dict] = None) -> list:
-        if _scoring_matrix == None:
-            _scoring_matrix = self.scoring_matrix
-        return [key for key, value in _scoring_matrix.items() if value >= max(_scoring_matrix.values()) * percentage]
+    def percentage_within_highest_score(self, percentage = 0.8, _Voting_matrix: Optional[dict] = None) -> list:
+        if _Voting_matrix == None:
+            _Voting_matrix = self.Voting_matrix
+        return [key for key, value in _Voting_matrix.items() if value >= max(_Voting_matrix.values()) * percentage]
     
     ## @brief
     # Find the list of entities that has score within percentage of total score.
     # @param percentage The percentage. For example, 0.8 means find all entities that has score at least 
     # 80 percent of total score.
     # @param _summed_weight Total weight. Default is self.summed_weight.
-    # @param _scoring_matrix Dictionary of word, score pair. Default is self.scoring_matrix.
+    # @param _Voting_matrix Dictionary of word, score pair. Default is self.Voting_matrix.
     # @return The list of entities.  
-    def minimum_weighted_score(self, percentage = 0.8, _summed_weight: Optional[float] = None, _scoring_matrix: Optional[dict] = None) -> list:
+    def minimum_weighted_score(self, percentage = 0.8, _summed_weight: Optional[float] = None, _Voting_matrix: Optional[dict] = None) -> list:
         if _summed_weight == None:
             _summed_weight = self.summed_weight
-        if _scoring_matrix == None:
-            _scoring_matrix = self.scoring_matrix
-        return[key for key, value in _scoring_matrix.items() if value >= _summed_weight * percentage]
+        if _Voting_matrix == None:
+            _Voting_matrix = self.Voting_matrix
+        return[key for key, value in _Voting_matrix.items() if value >= _summed_weight * percentage]
